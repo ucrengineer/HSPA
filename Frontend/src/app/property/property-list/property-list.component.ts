14c0fc0457264d19b7d7419fad3fe6664c0cf8b8
ucrengineerpy@gmail.com <ucrengineerpy@gmail.com>
2021-04-12 16:29:43 -0700

    added images + more properties to data model

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
 import { IProperty } from '../IProperty.interface';
 
@@ -9,19 +10,23 @@ import { IProperty } from '../IProperty.interface';
 })
 export class PropertyListComponent implements OnInit {
 
-
+  SellRent = 1;
 
   properties: Array<IProperty>;
 
 
-  constructor(private housingService:HousingService) { }
+  constructor(private housingService:HousingService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    if (this.route.snapshot.url.toString()){
+      this.SellRent = 2;
+    }
 
-    this.housingService.getAllProperties().subscribe(
+    this.housingService.getAllProperties(this.SellRent).subscribe(
         data=> {
         this.properties = data;
         console.log(data);
+        console.log(this.route.snapshot.url.toString());
         }, error => {
           console.log('httperror:');
         }

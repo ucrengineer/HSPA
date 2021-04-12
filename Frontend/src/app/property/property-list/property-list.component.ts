72386ae45d56daa5ccab986a8b34d9185c7b7d7a
ucrengineerpy@gmail.com <ucrengineerpy@gmail.com>
2021-04-12 09:30:53 -0700

    css + services added

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -9,63 +10,24 @@ export class PropertyListComponent implements OnInit {
 
 
 
-  properties: Array<any> =
-  [
+  properties: any;
 
-    {
-    "Id": 1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-    },
-    {
-      "Id": 2,
-      "Name":"Tennessee House",
-      "Type":"Apartment",
-      "Price":12000
-    },
-    {
-      "Id": 3,
-      "Name":"Mississippi House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id": 4,
-      "Name":"Brandon House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id": 5,
-      "Name":"Chicago House",
-      "Type":"Apartment",
-      "Price":12000
-    },
-    {
-      "Id": 6,
-      "Name":"Texas House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id": 7,
-      "Name":"Los Angeles House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id": 8,
-      "Name":"Mexico House",
-      "Type":"House",
-      "Price":12000
-    },
 
-  ]
-
-  constructor() { }
+  constructor(private housingService:HousingService) { }
 
   ngOnInit(): void {
+
+    this.housingService.getAllProperties().subscribe(
+        data=> {
+        this.properties = data;
+        console.log(data);
+        }, error => {
+          console.log(error);
+        }
+    )
+      }
+
+
   }
 
-}
+
